f8d42d4dda272593db32d17f908de647347e9f72
Lopez <lopezz@ny-gwc-02.ad.viacom.com>
2016-06-20 16:35:32 -0400

    Updated game. Fixing Brooklyn storyline bug

--- NYCAdventure.rb
+++ NYCAdventure.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require 'pry'
+
 =begin 
 This is where the player will begin 
 	Welcome message 
@@ -10,16 +12,21 @@ puts "You are in New York City! The concrete jungle where dreams are made and de
 
 puts "What is your name"
 first_name = gets.chomp
-puts "Hello, #{first_name}!"
+puts "Welcome, #{first_name}"
 
 puts"What borough would you like to visit? Choose carefully..."
 five_boroughs = gets.chomp
-puts "OK, so you're going to #{five_boroughs}. Good luck!"
+puts five_boroughs
+puts "You have entered at your own risk"
 	
 if five_boroughs.include?("Bronx") || five_boroughs.include?("bronx")
-	puts "FIVE_BOROUGHS VARIABLE IS NOW SET TO: #{five_boroughs}"
-	puts "You get on the 4 train going towards Woodlawn and you see a girl crying. Do you console her or do you ignore her?"
+	puts "You get on the 4 train going towards Woodlawn and you see a girl crying. Do you console her or do you ignore her"
 elsif
-	puts "5 train" 
+	puts "You get on the 5 train going towawrds New Lots Avenune. A poor man asks you for a dollar. Do you give him your change or do you ignore him?"
+	poor_man = gets.chomp
+	puts poor_man
 end 
 
+# if poor_man.include?("ignore")
+# 	puts"The man stabs you"
+# end 
